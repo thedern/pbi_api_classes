f17b27d03d24d3807eba6a6d39d0adfa70c0f949
thedern <dxsmith12@soverin.net>
2026-03-31 15:05:37 -0400

    Create console_themes.py

---
+++ support_utils/console_themes.py
@@ -0,0 +1,13 @@
+from rich.theme import Theme
+
+# https://rich.readthedocs.io/en/stable/style.html
+# https://rich.readthedocs.io/en/stable/appendix/colors.html#appendix-colors
+custom_theme = Theme(
+    {
+        "header": "cyan",
+        "info": "bold yellow",
+        "warning": "magenta",
+        "danger": "bold red",
+        "number": "bold dark_sea_green1",
+    }
+)
